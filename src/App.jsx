864041d3dd650cb1b4fdba70bc737c4f5233b95d
“Volodymyr <vtabashniuk@outlook.com>
2023-02-19 01:42:27 +0200

    add dugut buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
 
+const numbers = new Array(10)
+  .fill(0)
+  .map((i, index) => index);
+
 function App() {
   return (
     <>
       <h1>Calculator</h1>
+      {numbers.map((i) => (
+        <button type="submit" key={i}>
+          {i}
+        </button>
+      ))}
     </>
   );
 }
